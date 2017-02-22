d1e31548a844c09cbefc3b43507cbe14f10e7206
Kenichi Hasegawa <kenichi.hasegawa.1969@gmail.com>
2017-02-23 01:21:34 +0900

    Heroku用にproductionはpgに

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218082550) do
+ActiveRecord::Schema.define do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "zip_code"
