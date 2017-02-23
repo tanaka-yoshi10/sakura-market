2c2204c1113715cd47b4d6a4d76cb71a1397902c
Kenichi Hasegawa <kenichi.hasegawa.1969@gmail.com>
2017-02-23 16:17:54 +0900

    テスト用の最低限のデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 CodCharge.delete_all
-CodCharge.create(
-    [
-        {cod_charge_code:'01',charge:300,start_amount:0, end_amount:10000},
-        {cod_charge_code:'04',charge:400,start_amount:10000, end_amount:30000},
-        {cod_charge_code:'04',charge:1000,start_amount:30000, end_amount:100000},
-        {cod_charge_code:'04',charge:1000,start_amount:100000, end_amount:99999999}
-    ])
+CodCharge.create([
+  {cod_charge_code:'01',charge:300,start_amount:0, end_amount:10000},
+  {cod_charge_code:'04',charge:400,start_amount:10000, end_amount:30000},
+  {cod_charge_code:'04',charge:1000,start_amount:30000, end_amount:100000},
+  {cod_charge_code:'04',charge:1000,start_amount:100000, end_amount:99999999}
+])
+ShipTime.delete_all
+ShipTime.create([
+  {shiptime_code: '01', display_name: '8時〜12時'},
+  {shiptime_code: '02', display_name: '12時〜14時'},
+  {shiptime_code: '03', display_name: '14時〜16時'},
+  {shiptime_code: '04', display_name: '16時〜18時'},
+  {shiptime_code: '05', display_name: '18時〜20時'},
+  {shiptime_code: '06', display_name: '20時〜21時'},
+  {shiptime_code: '00', display_name: '未指定'},
+])
+User.delete_all
+User.create([
+  {email: 'admin@mail.jp', name: '管理者', password_digest: '$2a$10$BH.IQxzd3kEWbHAuHDEWgedl2XW/2zcSV82L0VVuU6DYZlBri77D2'}
+])
